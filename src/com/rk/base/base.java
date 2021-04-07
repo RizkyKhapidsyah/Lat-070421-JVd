1280d4d49c6f391e2cead75625ff40ee9b39d610
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-04-07 19:20:59 +0700

    Initial commit

---
+++ src/com/rk/base/base.java
@@ -0,0 +1,9 @@
+package com.rk.base;
+
+public class base {
+    public static String s = " Super Class ";
+
+    public base(){
+        System.out.println("1");
+    }
+}
---
+++ src/com/rk/lat040321.java
@@ -0,0 +1,16 @@
+package com.rk;
+
+import com.rk.base.base;
+
+public class lat040321 extends base {
+
+    public lat040321(){
+        System.out.println("2");
+        super(); //Compilation Error
+    }
+
+    public static void main(String[] args) {
+        lat040321 obj = new lat040321();
+        System.out.println(s);
+    }
+}
